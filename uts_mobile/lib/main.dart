763caf4d206a8cfd6026ee6611c74a3245fcf086
brilliyandddd <brilliyandnoval@gmail.com>
2023-11-01 16:20:17 +0700

    vneibfeihbwoiuefeubfuehf

--- uts_mobile/lib/main.dart
+++ uts_mobile/lib/main.dart
@@ -1,18 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello test!'),
+        body: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 60, left: 20),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 75,
+                height: 75,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 60),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, 1), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Transform.scale(
+                      scale: 1.2,
+                      child: Image.asset(
+                      'assets/images/discount.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    )
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    margin: EdgeInsets.only(top: 60, right: 30),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, 1), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Transform.scale(
+                      scale: 1.2,
+                      child: Image.asset(
+                      'assets/images/heart.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ],
+          ),
+          SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(top: 5),
+              width: 350,
+              height: 160,
+              decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [Colors.red[600]!, Colors.red[900]!],
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Hi, Brilliyand Noval Herdana Putra',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+            ),
+          ],
         ),
       ),
     );
