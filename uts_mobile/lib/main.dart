38e3917c9918589622f77768c21f5599751f97f7
brilliyandddd <brilliyandnoval@gmail.com>
2023-11-02 11:48:27 +0700

    2 white rectangel

--- uts_mobile/lib/main.dart
+++ uts_mobile/lib/main.dart
@@ -11,103 +11,136 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 60, left: 20),
-              child: Image.asset(
-                'assets/images/logo.png',
-                width: 75,
-                height: 75,
-              ),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 60),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 1,
-                          blurRadius: 1,
-                          offset: Offset(1, 1), // changes position of shadow
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Transform.scale(
-                      scale: 1.0,
-                      child: Image.asset(
-                      'assets/icons/coupon.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 60, left: 20),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 75,
+                    height: 75,
                   ),
-                  SizedBox(width: 10),
-                  Container(
-                    margin: EdgeInsets.only(top: 60, right: 30),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 1,
-                          blurRadius: 1,
-                          offset: Offset(1, 1), // changes position of shadow
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    child: Transform.scale(
-                      scale: 1.2,
-                      child: Image.asset(
-                      'assets/icons/heart.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    )
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.only(top: 60),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 1,
+                                offset:
+                                    Offset(1, 1), // changes position of shadow
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: Transform.scale(
+                            scale: 0.6,
+                            child: Image.asset(
+                              'assets/icons/coupon.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          )),
+                      SizedBox(width: 10),
+                      Container(
+                          margin: EdgeInsets.only(top: 60, right: 30),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 1,
+                                offset:
+                                    Offset(1, 1), // changes position of shadow
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: Transform.scale(
+                            scale: 0.6,
+                            child: Image.asset(
+                              'assets/icons/heart.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          )),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-          ),
-          SizedBox(height: 20),
+            SizedBox(height: 5),
             Container(
               margin: EdgeInsets.only(top: 5),
               width: 350,
               height: 160,
               decoration: BoxDecoration(
-              gradient: LinearGradient(
+                gradient: LinearGradient(
                   begin: Alignment.centerLeft,
                   end: Alignment.centerRight,
                   colors: [Colors.red[600]!, Colors.red[900]!],
                 ),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Align(
+              child: Stack(
+                children: [
+                  Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
-                      padding: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: Text(
-                        'Hi, Brilliyand Noval Herdana Putra',
+                        'Hai, BRILLIYAND NOVAL HERDANA PUTRA',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ),
+                  Positioned(
+                    left: 20,
+                    bottom: 30,
+                    child: Container(
+                      width: 130,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        ),
+                      ),
+                  ),
+                  Positioned(
+                    right: 55,
+                    bottom: 30,
+                    child: Container(
+                      width: 130,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
             ),
           ],
         ),
